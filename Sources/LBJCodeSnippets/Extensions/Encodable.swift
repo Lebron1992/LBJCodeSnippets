aff9af503bbcec99544fd1301e068399d03b2fba
Lebron <wenzhi.zeng@outlook.com>
2022-04-22 16:52:31 +0800

    mark as public

--- Sources/LBJCodeSnippets/Extensions/Encodable.swift
+++ Sources/LBJCodeSnippets/Extensions/Encodable.swift
@@ -3,7 +3,7 @@ import Foundation
 private let jsonEncoder = JSONEncoder()
 
 extension Encodable {
-    var dictionary: [String: Any] {
+    public var dictionary: [String: Any] {
         let obj = try? JSONSerialization.jsonObject(with: jsonEncoder.encode(self))
         return obj as? [String: Any] ?? [:]
     }
